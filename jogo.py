f250b7c871a66162834b08c9fbb61fc6e143564d
Bruno Brizolla Do Nascimento <104790790+MouroBr@users.noreply.github.com>
2023-01-21 15:16:37 -0300

    criando algumas classes e estado

---
+++ jogo.py
@@ -0,0 +1,13 @@
+class Participante:
+    def __init__(self):
+        self.ponto = 0
+        self.chances = ""
+
+class Rodadas:
+
+
+class Jogo:
+    def __init__(self):
+        self.finalJogo = False
+        self.participante01 = Participante()
+        self.participante02 = Participante()
